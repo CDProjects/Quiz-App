09aff0c269f2ef821cc3efa0b9c803c0f6a51974
Kiwicase <caseydentprojects@gmail.com>
2023-04-01 16:41:23 +0300

    Questions 1 & 2 added to JS

---
+++ script.js
@@ -0,0 +1,17 @@
+const quizData = [
+    {
+        question: 'How many Rugby World Cups has the Northern Hemishpere won since the inaugural tournament in 1987?',
+        a: '0',
+        b: '1',
+        c: '2',
+        d: '3',
+        correct: 'b',
+    }, {
+        question: 'which player has scored the most tries in a Rugby World Cup tournament?',
+        a: 'Jonny Wilkinson',
+        b: 'Bryan Habana',
+        c: 'Jonah Lomu',
+        d: 'Bryan Habana & Jonah Lomu',
+        correct: 'd',
+    }
+]
