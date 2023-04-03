6bec8bfdb8d9647b9993f16f1006e032ad722e1f
Kiwicase <caseydentprojects@gmail.com>
2023-04-03 11:18:38 +0300

    function amended in JS, variables added

--- script.js
+++ script.js
@@ -60,6 +60,8 @@ const quizData = [
     }
 ];
 
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
@@ -68,6 +70,7 @@ const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit")
 
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
@@ -81,3 +84,5 @@ function loadQuiz() {
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
 }
+
+sub
